faf50bc1126699a21cec467cda1b7160330f02d6
Misho <chapidze.misho95@gmail.com>
2023-08-23 23:08:00 +0400

    s

--- src/components/singin/mobile.side.tsx
+++ src/components/singin/mobile.side.tsx
@@ -1,10 +1,13 @@
 import mobileBg from "../../assets/images/cover-How-to-use-Instagram.png";
+import innerImg from "../../assets/images/the_spines-411x800.jpg";
 
 const MobileSide = () => {
   return (
     <div className="relative w-80">
       <img src={mobileBg} className="absolute z-50" />
-      <div className="absolute left-20 h-mobile w-56 top-5 px-3">text</div>
+      <div className="absolute left-20 h-mobile w-56 top-5 px-3">
+        <img src={innerImg} className="w-full pt-7" />
+      </div>
     </div>
   );
 };
