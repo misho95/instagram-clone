a524de9377feecd24df23482d07b9cc4b4019011
Misho <chapidze.misho95@gmail.com>
2023-08-24 23:18:30 +0400

    ss

--- src/components/home.components/leftNavBar.tsx
+++ src/components/home.components/leftNavBar.tsx
@@ -10,12 +10,15 @@ const LeftNavBar = () => {
   }
 
   return (
-    <div className="fixed border-r-px1 border-gray-200 z-50 bg-red-500 p-6 w-full h-20 bottom-0 sm:w-fit sm:h-screen flex flex-row sm:flex-col justify-around sm:justify-start gap-5">
-      <NavBarLinks />
-      <NavBarLinks />
-      <NavBarLinks />
-      <NavBarLinks />
-      <NavBarLinks />
+    <div className="fixed border-t-px1 sm:border-r-px1 border-gray-200 z-50 p-6 w-full h-20 bottom-0 sm:w-fit sm:h-screen flex flex-row sm:flex-col justify-around sm:justify-start gap-5">
+      <NavBarLinks icons={"home"} name={"Home"} />
+      <NavBarLinks icons={"search"} name={"Search"} />
+      <NavBarLinks icons={"explore"} name={"Explore"} />
+      <NavBarLinks icons={"movie"} name={"Reels"} />
+      <NavBarLinks icons={"chat"} name={"Messages"} />
+      <NavBarLinks icons={"favorite"} name={"Notifications"} />
+      <NavBarLinks icons={"add_box"} name={"Create"} />
+      <NavBarLinks icons={"account_circle"} name={"Profile"} />
     </div>
   );
 };
--- src/components/home.components/nav.bar.links.tsx
+++ src/components/home.components/nav.bar.links.tsx
@@ -1,12 +1,10 @@
 import { Link } from "react-router-dom";
 
-const NavBarLinks = () => {
-  const open = true;
-
+const NavBarLinks = ({ icons, name }) => {
   return (
     <Link to="/" className="flex gap-5 items-center">
-      <span className="material-symbols-outlined">favorite</span>
-      {!open && <span className="hidden sm:block">Link Name</span>}
+      <span className="material-symbols-outlined">{icons}</span>
+      <span className="hidden lg:block">{name}</span>
     </Link>
   );
 };
