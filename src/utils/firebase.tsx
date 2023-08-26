f4bf93075b2eed089811943f56b392839fb50acb
Misho <chapidze.misho95@gmail.com>
2023-08-26 13:39:22 +0400

    test 25

--- src/utils/firebase.tsx
+++ src/utils/firebase.tsx
@@ -104,22 +104,25 @@ export const authWithFacebookPopUp = async () => {
     .then((result) => {
       // The signed-in user info.
       const user = result.user;
+      console.log(user);
 
       // This gives you a Facebook Access Token. You can use it to access the Facebook API.
       const credential = FacebookAuthProvider.credentialFromResult(result);
+      console.log(credential);
       // const accessToken = credential.accessToken;
 
       // IdP data available using getAdditionalUserInfo(result)
       // ...
     })
     .catch((error) => {
+      console.log(error);
       // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.customData.email;
-      // The AuthCredential type that was used.
-      const credential = FacebookAuthProvider.credentialFromError(error);
+      // const errorCode = error.code;
+      // const errorMessage = error.message;
+      // // The email of the user's account used.
+      // const email = error.customData.email;
+      // // The AuthCredential type that was used.
+      // const credential = FacebookAuthProvider.credentialFromError(error);
 
       // ...
     });
